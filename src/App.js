ca02c65e56f03abe987367f0b5f42d090b140f10
yongsung <mirr001003@hanyang.ac.kr>
2023-08-05 04:08:49 +0900

    query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainProducts from './components/MainProducts';
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return  ( 
+  <QueryClientProvider client={queryClient}>
+    <MainProducts />;
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+  )
 }
-
-export default App;
